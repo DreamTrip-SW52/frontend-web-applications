a9caa5f7cd90758fa09586265cef283e21f76abe
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2022-10-21 14:24:57 -0500

    feat: services to get, delete economics

--- src/services/CurrentTravel.service.js
+++ src/services/CurrentTravel.service.js
@@ -1,9 +1,44 @@
 import http from './common';
 
 export class CurrentTravelService {
-  async getByTravelerId(travelerId) {
-    const { data } = await http.get(`/current-travels/?travelerId=${travelerId}`);
+  async getCurrentTravel(travelerId) {
+    const response = await http.get(`/current-travels/?travelerId=${travelerId}`);
 
-    return http.get(`/packages/${data?.[0]?.packageId}`);
+    return response?.data?.[0];
+  }
+
+  async getByTravelerId(travelerId) {
+    const currentTravel = await this.getCurrentTravel(travelerId);
+
+    return http.get(`/packages/${currentTravel?.packageId}`);
+  }
+
+  async createNewBill(travelerId, newBill) {
+    const currentTravel = await this.getCurrentTravel(travelerId);
+    currentTravel?.new_bills?.push({
+      ...newBill,
+      id: currentTravel?.new_bills?.length + 1,
+    });
+
+    await http.put(`/current-travels/${currentTravel?.id}`, { ...currentTravel });
+  }
+
+  async deleteBill(travelerId, billId) {
+    const currentTravel = await this.getCurrentTravel(travelerId);
+    const idx = currentTravel?.new_bills?.findIndex(bill => bill?.id === billId);
+    currentTravel?.new_bills?.splice(idx, 1);
+
+    await http.put(`/current-travels/${currentTravel?.id}`, { ...currentTravel });
+  }
+
+  async editBill(travelerId, billId, modifiedBill) {
+    const currentTravel = await this.getCurrentTravel(travelerId);
+    const idx = currentTravel?.new_bills?.findIndex(bill => bill?.id === billId);
+    currentTravel.new_bills[idx] = {
+      ...modifiedBill,
+      id: currentTravel?.new_bills?.[idx]?.id,
+    };
+
+    await http.put(`/current-travels/${currentTravel?.id}`, { ...currentTravel });
   }
 }
